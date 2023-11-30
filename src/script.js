8350c1f8b79bcb8d0f52df58111af3978b6ea2e7
lucas-landin <geada_n@hotmail.com>
2023-11-29 23:07:20 -0300

    glass look

--- src/script.js
+++ src/script.js
@@ -89,21 +89,23 @@ const gradientTexture = textureLoader.load('./textures/gradients/5.jpg')
 // gui.add(material, 'aoMapIntensity').min(0).max(2).step(0.0001)
 // gui.add(material, 'displacementScale').min(0).max(1).step(0.0001)
 
-// MeshPhysicaldMaterial
+// MeshPhysicaldMaterial worts material for performance 
 const material = new THREE.MeshPhysicalMaterial()
-material.metalness = 1
-material.roughness = 1
-material.map = doorColorTexture
-material.aoMap = doorAmbientOcclusionTexture
-material.aoMapIntensity = 1
-material.displacementMap = doorHeightTexture
-material.displacementScale = 0.0782
-material.metalnessMap = doorMetalnessTexture
-material.roughnessMap = doorRoughnessTexture
-material.normalMap = doorNormalTexture
-material.normalScale.set(0.5, 0.5)
-material.transparent = true
-material.alphaMap = doorAlphaTexture
+material.metalness = 0
+material.roughness = 0
+// material.map = doorColorTexture
+// material.aoMap = doorAmbientOcclusionTexture
+// material.aoMapIntensity = 1
+// material.displacementMap = doorHeightTexture
+// material.displacementScale = 0.0782
+// material.metalnessMap = doorMetalnessTexture
+// material.roughnessMap = doorRoughnessTexture
+// material.normalMap = doorNormalTexture
+// material.normalScale.set(0.5, 0.5)
+// material.transparent = true
+// material.alphaMap = doorAlphaTexture
+
+//Vernish efect
 //material.clearcoat = 1
 //material.clearcoatRoughness = 0
 //For fluffy material ex fabric
@@ -111,15 +113,40 @@ material.alphaMap = doorAlphaTexture
 // material.sheenRoughness = 0.25
 // material.sheenColor.set(1, 1, 1)
 
-gui.add(material, 'metalness').min(0).max(1).step(0.0001)
+//gui.add(material, 'metalness').min(0).max(1).step(0.0001)
 gui.add(material, 'roughness').min(0).max(1).step(0.0001)
-gui.add(material, 'aoMapIntensity').min(0).max(2).step(0.0001)
-gui.add(material, 'displacementScale').min(0).max(1).step(0.0001)
+//gui.add(material, 'aoMapIntensity').min(0).max(2).step(0.0001)
+//gui.add(material, 'displacementScale').min(0).max(1).step(0.0001)
+
 //gui.add(material, 'clearcoat').min(0).max(1).step(0.0001)
 //gui.add(material, 'clearcoatRoughness').min(0).max(1).step(0.0001)
+
 // gui.add(material, 'sheen').min(0).max(1).step(0.0001)
 // gui.add(material, 'sheenRoughness').min(0).max(1).step(0.0001)
 // gui.addColor(material, 'sheenColor')
+
+
+// Iridescence
+// material.iridescence = 1
+// material.iridescenceIOR = 1
+// material.iridescenceThicknessRange = [ 100, 800 ]
+
+// gui.add(material, 'iridescence').min(0).max(1).step(0.0001)
+// gui.add(material, 'iridescenceIOR').min(1).max(2.333).step(0.0001)
+// gui.add(material.iridescenceThicknessRange, '0').min(1).max(1000).step(1)
+// gui.add(material.iridescenceThicknessRange, '1').min(1).max(1000).step(1)
+
+
+// Transmission looks like theree is liquid in it 
+material.transmission = 1
+// Ior = index of refraction changes on what type of material simulated 
+// Diamond:2.471 water: 1.333 air: 1.000293 etc
+material.ior = 1.5 
+material.thickness = 0.5
+
+ gui.add(material, 'transmission').min(0).max(1).step(0.0001)
+ gui.add(material, 'ior').min(1).max(10).step(0.0001)
+ gui.add(material, 'thickness').min(0).max(1).step(0.0001)
 
 
 
